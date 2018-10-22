4d007082910488dbd02b2b7dc59d93bbf4749110
LeDaVR <danielvaldiviaramos@hotmail.com>
2018-10-22 16:09:56 -0500

    re

--- cap8d/punteroafuncion.cpp
+++ cap8d/punteroafuncion.cpp
@@ -1,19 +1,15 @@
 #include <iostream>
 using namespace std;
-void ascendente(int *a,int len){
-	for(int i=0;i<len;i++)
-		for(int j=i+1;j<len;j++){
-			if(*(a+i)>*(a+j)){
-				int temp=*(a+i);
-				*(a+i)=*(a+j);
-				*(a+j)=temp;
-			}
-		}
+bool ascendente(int a,int b){
+	return a>b;
 }
-void descendente(int *a,int len){
+bool descendente(int a,int b){
+	return a<b;
+}
+void ordenamiento(int *a,int len,bool (*funpo) (int,int)){
 	for(int i=0;i<len;i++)
 		for(int j=i+1;j<len;j++){
-			if(*(a+i)<*(a+j)){
+			if((*funpo) (*(a+i),*(a+j))){
 				int temp=*(a+i);
 				*(a+i)=*(a+j);
 				*(a+j)=temp;
@@ -22,15 +18,7 @@ void descendente(int *a,int len){
 }
 int main(){
 	int array[10]={9,8,7,4,5,6,1,2,3,10};
-	void (*funpo) (int *,int);
-	cout<<"ordenamiento descendente o ascendente"<<endl;
-	string b;
-	cin>>b;
-	if(b=="ascendente"){
-		funpo=ascendente;
-	}else
-		funpo=descendente;
-	funpo(array,10);
+	ordenamiento(array,10,descendente);
 	for(int i=0;i<10;i++)
 		cout<<*(array+i)<<" ";
 	return 0;
